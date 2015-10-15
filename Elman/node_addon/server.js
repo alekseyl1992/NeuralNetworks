f02c1e73950bdeb39fc25de87abd65e1c5da9149
alekseyl1992 <alekseyl@list.ru>
2015-10-15 12:08:42 +0300

    JS code prepared

--- Elman/node_addon/server.js
+++ Elman/node_addon/server.js
@@ -1,19 +1,77 @@
 var addon = require('./build/Release/addon');
 
-var nConfig = {
-
-};
-
 var gConfig = {
-
+    populationSize: 2,
+    targetFitness: 0.1,
+    mutationProb: 0.1,
 };
 
-var trainSet = {
 
+var nConfig = {
+    inputsCount: 9,
+    hiddenCount: 9,
 };
 
-var obj = new addon.GeneticNodeWrapper(nConfig, gConfig, trainSet);
-obj.start((iterationsCount) => console.log("Finished after: " + iterationsCount));
-obj.recognize({});
+var sample1 = [
+    [
+        [
+            -1, -1, -1,
+             1, -1, -1,
+            -1, -1, -1
+        ],
+        [
+            -1, -1, -1,
+            -1,  1, -1,
+            -1, -1, -1
+        ],
+        [
+            -1, -1, -1,
+            -1, -1,  1,
+            -1, -1, -1
+        ],
+        [
+            -1, -1, -1,
+            -1, -1, -1,
+            -1, -1, -1
+        ],
+    ],
+    1
+];
+
+var sample2 = [
+    [
+        [
+            -1, -1, -1,
+            -1, -1,  1,
+            -1, -1, -1
+        ],
+        [
+            -1, -1, -1,
+            -1,  1, -1,
+            -1, -1, -1
+        ],
+        [
+            -1, -1, -1,
+             1, -1, -1,
+            -1, -1, -1
+        ],
+        [
+            -1, -1, -1,
+            -1, -1, -1,
+            -1, -1, -1
+        ],
+    ],
+    -1
+];
+
+var trainSet = [sample1, sample2];
+
+var obj = new addon.GeneticNodeWrapper(gConfig, nConfig, trainSet);
+obj.start((iterationsCount) => {
+    console.log("Finished after: " + iterationsCount);
+
+    console.log(obj.recognize(sample1[0]));
+    console.log(obj.recognize(sample2[0]));
+});
 
 setTimeout(() => console.log('Timed out'), 5000);
